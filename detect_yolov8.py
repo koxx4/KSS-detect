d0c05bef54c4e85647d21350f456c42be1b2f387
koxx4 <piotr815@protonmail.com>
2023-10-22 16:13:35 +0200

    code cleaning

--- detect_yolov8.py
+++ detect_yolov8.py
@@ -1,9 +1,6 @@
 import cv2
-import time
 from ultralytics import YOLO
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
-from picamera2.outputs import CircularOutput
 
 model = YOLO("yolov5nu-v7-b-q.onnx")
 picam2 = Picamera2()
@@ -13,15 +10,13 @@ picam2.configure(picam2.create_video_configuration(
 picam2.start()
 
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('filename.avi', fourcc, 10, (1920, 1080))
+out = cv2.VideoWriter('inference.avi', fourcc, 10, (1920, 1080))
 
 while True:
     im = picam2.capture_array()
 
     if not im.any():
         continue
-    # rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-    # resized = cv2.resize(rgb, (640, 360), interpolation = cv2.INTER_AREA)
-    results = model(source=im)
 
+    results = model(source=im)
     out.write(results[0].plot())
