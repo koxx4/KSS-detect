207d6e6777059c81ec70f2d53e598e6b9835655a
koxx4 <piotr815@protonmail.com>
2023-11-19 22:53:25 +0100

    added read prop, removed unused pickle import

--- event.py
+++ event.py
@@ -10,7 +10,8 @@ class KssEvent:
                  confidence: float,
                  date=None,
                  important=False,
-                 bounding_boxes=None):
+                 bounding_boxes=None,
+                 read=False):
         self.name = name
         self.count = count
         self.bounding_boxes = bounding_boxes
@@ -18,6 +19,7 @@ class KssEvent:
         self.confidence = confidence
         self.date = date if date is not None else datetime.now(tz=timezone.utc)
         self.important = important
+        self.read = read
 
     def __str__(self):
         bounding_boxes_str = ', '.join(f"({x}, {y}, {w}, {h})" for x, y, w, h in (self.bounding_boxes or []))
@@ -29,4 +31,5 @@ class KssEvent:
                 f"  Image Shape: {image_shape}\n"
                 f"  Confidence: {self.confidence:.2f}\n"
                 f"  Date: {self.date}\n"
-                f"  Important: {self.important}\n)")
+                f"  Important: {self.important}\n"
+                f"  Read: {self.read}\n)")
